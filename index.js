35a906655690e7d286d54752274c604461fac1b5
Japneet Singh <japneetsingh1807@outlook.it>
2024-06-03 13:59:00 +0200

    WIP exercises

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let persons = [
 
 app.use(express.json())
 
-app.get('/api/info', (request, response) => {
+app.get('/info', (request, response) => {
     const now = new Date()
     let resultHtml = `<p>Phonebook has info for ${persons.length} people<p><p>${now.toString()}`
     response.send(resultHtml)
@@ -34,6 +34,55 @@ app.get('/api/info', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+  console.log(persons.find(p => p.name === body.name))
+  if (persons.find(p => p.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(note => note.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
